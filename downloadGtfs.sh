9ccf10e5fbf4fa3017f988d926d28baf5ae9eb0c
Mael <laem@kont.me>
2024-01-12 11:53:03 +0100

    Oubli du fichier OSM

--- downloadGtfs.sh
+++ downloadGtfs.sh
@@ -3,3 +3,5 @@ wget https://eu.ftp.opendatasoft.com/sncf/gtfs/export-ter-gtfs-last.zip -O input
 wget https://eu.ftp.opendatasoft.com/sncf/gtfs/export-intercites-gtfs-last.zip -O input/intercites.gtfs.zip
 wget https://eu.ftp.opendatasoft.com/sncf/gtfs/export_gtfs_voyages.zip -O input/tgv.gtfs.zip
 
+wget https://download.geofabrik.de/europe/france/bretagne-latest.osm.pbf -O input/bretagne.osm.pbf
+
