30a9aca8a59686e1ecfc87e4d2a118e651fa7aea
Mael <laem@kont.me>
2024-01-12 16:09:52 +0100

    Faut installer Deno

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: deno run --allow-net --allow-env --allow-run server.ts
+web: ./denoSetup.sh 
---
+++ denoSetup.sh
@@ -0,0 +1,4 @@
+curl -fsSL https://deno.land/install.sh | sh
+deno run --allow-net --allow-env --allow-run server.ts
+
+
