0386be20ff3f2ed0ca70a09882b1cbbab59375fd
Mael <laem@kont.me>
2024-07-14 20:55:12 +0200

    On évite des problèmes de cache

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd /root/motis
+#I had problems with flixbus and blabla, couldn't know where it came from
+#since nigiri is very fast, we remove its cache just in case
+rm ./data/nigiri/*
 ./motis/motis --server.port 3000
