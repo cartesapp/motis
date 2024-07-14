7bae5dc1306d4c565a38a2f2573f3ff4c7e76c6c
Mael <laem@kont.me>
2024-07-14 21:48:48 +0200

    Ajout de la normandie aux données OSM pour OSRM

--- downloadOsm.sh
+++ downloadOsm.sh
@@ -2,7 +2,9 @@ wget https://download.geofabrik.de/europe/france/bretagne-latest.osm.pbf -O inpu
 wget https://download.geofabrik.de/europe/france/pays-de-la-loire-latest.osm.pbf -O input/pays-de-la-loire.osm.pbf
 wget https://download.geofabrik.de/europe/france/auvergne-latest.osm.pbf -O input/auvergne.osm.pbf
 wget https://download.geofabrik.de/europe/france/rhone-alpes-latest.osm.pbf -O input/rhone-alpes.osm.pbf
+wget https://download.geofabrik.de/europe/france/basse-normandie-latest.osm.pbf -O input/basse-normandie.osm.pbf
+wget https://download.geofabrik.de/europe/france/haute-normandie-latest.osm.pbf -O input/haute-normandie.osm.pbf
 
 cd input
-osmium merge bretagne.osm.pbf pays-de-la-loire.osm.pbf auvergne.osm.pbf rhone-alpes.osm.pbf -o cartes.osm.pbf
+osmium merge bretagne.osm.pbf pays-de-la-loire.osm.pbf auvergne.osm.pbf rhone-alpes.osm.pbf haute-normandie.osm.pbf basse-normandie.osm.pbf -o cartes.osm.pbf --overwrite
 cd ..
