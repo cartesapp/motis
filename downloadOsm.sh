de8a62ef6c2f264c6e5623449a6a3214a985357e
Mael <laem@kont.me>
2024-07-16 22:22:05 +0200

    Ajout de l'IdF pour les transferts train en vélo à Paris

--- downloadOsm.sh
+++ downloadOsm.sh
@@ -4,7 +4,8 @@ wget https://download.geofabrik.de/europe/france/auvergne-latest.osm.pbf -O inpu
 wget https://download.geofabrik.de/europe/france/rhone-alpes-latest.osm.pbf -O input/rhone-alpes.osm.pbf
 wget https://download.geofabrik.de/europe/france/basse-normandie-latest.osm.pbf -O input/basse-normandie.osm.pbf
 wget https://download.geofabrik.de/europe/france/haute-normandie-latest.osm.pbf -O input/haute-normandie.osm.pbf
+wget https://download.geofabrik.de/europe/france/ile-de-france-latest.osm.pbf -O input/idf.osm.pbf
 
 cd input
-osmium merge bretagne.osm.pbf pays-de-la-loire.osm.pbf auvergne.osm.pbf rhone-alpes.osm.pbf haute-normandie.osm.pbf basse-normandie.osm.pbf -o cartes.osm.pbf --overwrite
+osmium merge bretagne.osm.pbf pays-de-la-loire.osm.pbf auvergne.osm.pbf rhone-alpes.osm.pbf haute-normandie.osm.pbf basse-normandie.osm.pbf idf.osm.pbf -o cartes.osm.pbf --overwrite
 cd ..
